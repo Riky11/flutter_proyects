23a850d1a7f0496800f61a4daeafd6f99b69406d
Ricardo Espinosa <ricardo.martinez@nunsys.com>
2025-09-23 16:00:54 +0200

    VIDEO 129

--- expense_tracker/lib/widgets/new_expese.dart
+++ expense_tracker/lib/widgets/new_expese.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 
 class NewExpese extends StatefulWidget {
-  const NewExpese({super.key});
+  const NewExpese({super.key, required this.onAddExpense});
+
+  final Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpese> createState() {
@@ -13,11 +17,59 @@ class NewExpese extends StatefulWidget {
 class _NewExpeseState extends State<NewExpese> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-  
-  void _presentDatePicker() {
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
-    showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: now);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Invalid Input'),
+          content: Text(
+            'Please make sure a valid title, amount, date and category was entered.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: Text('Ok'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    final newExpense = Expense(
+      title: _titleController.text,
+      amount: enteredAmount,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    );
+
+    widget.onAddExpense(newExpense);
+    Navigator.pop(context);
   }
 
   @override
@@ -30,7 +82,7 @@ class _NewExpeseState extends State<NewExpese> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsetsGeometry.all(16),
+      padding: EdgeInsetsGeometry.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
@@ -38,47 +90,72 @@ class _NewExpeseState extends State<NewExpese> {
             maxLength: 50,
             decoration: InputDecoration(label: Text('Tile')),
           ),
-          Row(children: [            
-            Expanded(
-              child: TextField(
-                controller: _amountController,
-                maxLength: 50,
-                decoration: InputDecoration(
-                  label: Text('Aomunt'),
-                  prefixText: '\$ ',
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            SizedBox(width: 16),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Selected Date'),
-                  IconButton(
-                    onPressed: _presentDatePicker,
-                    icon: Icon(Icons.calendar_month)
-                  )
-                ],
-              ),
-            ),
-          ],),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  decoration: InputDecoration(
+                    label: Text('Aomunt'),
+                    prefixText: '\$ ',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No selected date'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
-                }, 
-                child: Text('Cancel')
+                },
+                child: Text('Cancel'),
               ),
               SizedBox(width: 5),
               ElevatedButton(
-                onPressed: () {
-                  print('Title: ${_titleController.text}');
-                  print('Amount: ${_amountController.text}');
-                },
+                onPressed: _submitExpenseData,
                 child: Text('Save Expense'),
               ),
             ],
