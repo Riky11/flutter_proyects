5680785d6df26285b0ddd6d5a9f3d0df9c89574b
DESKTOP-REM\riky9 <riky11.esp@gmail.com>
2025-09-23 17:27:30 +0200

    responsive video 137

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorSheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125)
+  );
+
 void main() {
+  // ******   Bloquear giro de pantalla    ***************
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp
+  // ]).then((fn) {
+
+  // }); 
+  // *****************************************************
+
+  
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorSheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorSheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorSheme.primaryContainer,
+            foregroundColor: kDarkColorSheme.onPrimaryContainer
+          ),
+        ),
+      ),
+
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
@@ -32,6 +63,7 @@ void main() {
           ),
         ),
       ),
+      //themeMode: ThemeMode.system, //default
       home: Expenses(),
     ),
   );
--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -6,7 +6,7 @@ final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category {food, travel, leisure, work}
+enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
@@ -20,9 +20,8 @@ class Expense {
     required this.title,
     required this.amount,
     required this.date,
-    required this.category
+    required this.category,
   }) : id = uuid.v4();
-
 
   final String id;
   final String title;
@@ -32,5 +31,27 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
